0f0343751ee2cc549a98fb06735cb6f0215973fd
IanRibaltaGene <ian.ribalta@gmail.com>
2022-12-10 23:05:00 +0100

    Init

---
+++ MyClimate/src/main/kotlin/com/project2/MyClimate/MyClimateApplication.kt
@@ -0,0 +1,11 @@
+package com.project2.MyClimate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyClimateApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyClimateApplication>(*args)
+}
