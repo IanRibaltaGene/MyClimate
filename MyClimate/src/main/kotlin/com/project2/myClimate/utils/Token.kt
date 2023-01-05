483f9312c3d1709056e3edee9e4028578ed7a093
IanRibaltaGene <ian.ribalta@gmail.com>
2023-01-05 21:01:08 +0100

    Resolved deprecation issues

--- MyClimate/src/main/kotlin/com/project2/myClimate/utils/Token.kt
+++ MyClimate/src/main/kotlin/com/project2/myClimate/utils/Token.kt
@@ -6,20 +6,25 @@ import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
+import java.security.Key
 import java.util.*
+import javax.crypto.KeyGenerator
 
 @Component
-class Token(@Value ("\${jwt.secret}") val secret: String,
-    @Value("\${jwt.expiration}") val expiration: Long
+class Token(@Value("\${jwt.expiration:3600}") val expiration: Long
 ) {
     fun generateToken(userDetails: UserDetails): String {
         val claims = HashMap<String, Any>()
+        val keyGenerator = KeyGenerator.getInstance("AES")
+        keyGenerator.init(256)
+        val secret : Key
+        secret = keyGenerator.generateKey()
         return Jwts.builder()
             .setClaims(claims)
             .setSubject(userDetails.username)
             .setIssuedAt(Date(System.currentTimeMillis()))
             .setExpiration(Date(System.currentTimeMillis() + expiration * 1000))
-            .signWith(SignatureAlgorithm.HS512, secret)
+            .signWith(secret, SignatureAlgorithm.HS512)
             .compact()
     }
 }
